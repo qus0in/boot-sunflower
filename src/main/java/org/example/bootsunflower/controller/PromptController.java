9e8f57e437e6fb3879122133f23cb524cfca48cd
qus0in <qus0in@gmail.com>
2025-06-11 13:10:20 +0900

    일단 코드는 완료

--- src/main/java/org/example/bootsunflower/controller/PromptController.java
+++ src/main/java/org/example/bootsunflower/controller/PromptController.java
@@ -5,19 +5,20 @@ import org.example.bootsunflower.dto.PromptForm;
 import org.example.bootsunflower.entity.Prompt;
 import org.example.bootsunflower.service.GeminiService;
 import org.example.bootsunflower.service.PromptService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.io.IOException;
-
 @Controller
 @RequiredArgsConstructor
 public class PromptController {
     private final PromptService promptService;
     private final GeminiService geminiService;
+    @Value("${server.baseurl}")
+    private String baseUrl;
 
     @GetMapping
     public String index(Model model) {
@@ -27,19 +28,21 @@ public class PromptController {
 
     @PostMapping
     public String submit(PromptForm promptForm, Model model) {
-         String result = geminiService.generate(promptForm.text());
+        String result = geminiService.generate(promptForm.text());
         model.addAttribute("promptText", result);
         Prompt data = promptService.savePrompt(
                 promptForm.text(),
                 result
         );
         model.addAttribute("promptData", data);
+        model.addAttribute("baseUrl", baseUrl);
         return "index";
     }
 
     @GetMapping("/history/{id}")
     public String history(@PathVariable("id") String id, Model model) {
         model.addAttribute("promptData", promptService.getPromptById(id));
+        model.addAttribute("baseUrl", baseUrl);
         return "history";
     }
 }
