786cd5566fd211ad80a5db3e97a4ccb86f0fd79b
qus0in <qus0in@gmail.com>
2025-06-13 11:48:27 +0900

    Config

---
+++ src/main/java/org/example/bootsunflower/auth/SecurityConfig.java
@@ -0,0 +1,11 @@
+package org.example.bootsunflower.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
